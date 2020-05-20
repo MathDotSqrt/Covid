b2a4c98a3055e38b2ad25622f4cff504da983144
MathDotSqrt <ctrenkov@gmail.com>
2020-05-20 10:28:42 -0400

    Converted project to C++14

--- Covid/Stat.cpp
+++ Covid/Stat.cpp
@@ -40,33 +40,33 @@ Stat::Statistic Stat::Statistic::operator*(const f32 &rhs) const {
 	};
 }
 
-Stat::Statistic Stat::sqrt(const Stat::Statistic &s) {
-	return {
-		std::sqrt(s.num_susceptible),
-		std::sqrt(s.num_removed),
-		std::sqrt(s.num_steps)
-	};
-}
-
-Stat::Statistic Stat::sample_mean(const std::vector<Stat::Statistic> &stats) {
-	const auto sum = std::reduce(stats.begin(), stats.end(), Stat::Statistic());
-	return sum / stats.size();
-}
-
-Stat::Statistic Stat::sample_variance(const Stat::Statistic &sample_mean, const std::vector<Stat::Statistic> &stats) {
-	const auto square_error = [sample_mean](const auto &value) {
-		const auto delta = value - sample_mean;
-		return delta * delta;
-	};
-
-	const auto reduce = [&](const auto prev, const auto s2) {
-		const auto error = square_error(s2);
-		return prev + error;
-	};
-
-	const auto total_square_error = std::accumulate(stats.begin(), stats.end(), Stat::Statistic(), reduce);
-	return total_square_error / (stats.size() - 1);
-}
+//Stat::Statistic Stat::sqrt(const Stat::Statistic &s) {
+//	return {
+//		std::sqrt(s.num_susceptible),
+//		std::sqrt(s.num_removed),
+//		std::sqrt(s.num_steps)
+//	};
+//}
+//
+//Stat::Statistic Stat::sample_mean(const std::vector<Stat::Statistic> &stats) {
+//	const auto sum = std::reduce(stats.begin(), stats.end(), Stat::Statistic());
+//	return sum / stats.size();
+//}
+//
+//Stat::Statistic Stat::sample_variance(const Stat::Statistic &sample_mean, const std::vector<Stat::Statistic> &stats) {
+//	const auto square_error = [sample_mean](const auto &value) {
+//		const auto delta = value - sample_mean;
+//		return delta * delta;
+//	};
+//
+//	const auto reduce = [&](const auto prev, const auto s2) {
+//		const auto error = square_error(s2);
+//		return prev + error;
+//	};
+//
+//	const auto total_square_error = std::accumulate(stats.begin(), stats.end(), Stat::Statistic(), reduce);
+//	return total_square_error / (stats.size() - 1);
+//}
 
 void Stat::print_stat(const Statistic &s) {
 	std::cout << "susceptible: " << s.num_susceptible << " removed: " << s.num_removed << " steps: " << s.num_steps << '\n';
--- Covid/Stat.h
+++ Covid/Stat.h
@@ -31,10 +31,10 @@ namespace Stat {
 		}
 	};
 
-	Statistic sqrt(const Statistic &s);
-	
-	Statistic sample_mean(const std::vector<Statistic> &stats);
-	Statistic sample_variance(const Statistic &sample_mean, const std::vector<Statistic> &stats);
+	//Statistic sqrt(const Statistic &s);
+	//
+	//Statistic sample_mean(const std::vector<Statistic> &stats);
+	//Statistic sample_variance(const Statistic &sample_mean, const std::vector<Statistic> &stats);
 	
 	template<typename T>
 	T t_half_interval(f32 t_value, T std, int n) {
